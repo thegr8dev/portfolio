391cf99e9e3bfb2fb5b2aedf0d5ac7ed001e60f3
thegr8dev <ankitswonders@gmail.com>
2025-12-12 12:49:37 +0530

    Fix too long key issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
         URL::forceScheme('https');
     }
 }
